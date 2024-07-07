faf697ba1799b80ead90508095f2aa41b3692dd3
DevDenLab <tjoshi@student.concordia.ab.ca>
2024-07-07 13:18:20 -0600

    create base,navigation,footer and home to make use of jinja and added about us endpoint and inserted about us template

--- app/myapp/urls.py
+++ app/myapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/meetings', views.meeting, name='meeting'),
+    path('meetings', views.meeting, name='meeting'),
+    path('meetingsdetails', views.meeting_details, name='meeting_details'),
+    path('about', views.aboutus, name='aboutus')
+    
 ]
--- app/myapp/views.py
+++ app/myapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'Home.html')
 
 def meeting(request):
     return render(request, 'meetings.html')
+
+
+def meeting_details(request):
+    return render(request, 'meeting-details.html')
+
+def aboutus(request):
+    return render(request, 'Aboutus.html')
