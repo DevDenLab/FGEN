18381777d81ddef182f4fab0d73dcb2a62cce750
bhavinnirmal29 <bhavinnirmal29@gmail.com>
2024-07-08 13:11:55 -0600

    Navbar Links, views changed

--- app/myapp/urls.py
+++ app/myapp/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('meetingsdetails', views.meeting_details, name='meeting_details'),
     path('about', views.aboutus, name='aboutus'),
     path('contact', views.contactus, name='contactus'),
-    
+    path('events', views.events, name='events'),
+    path('getinvolved', views.GetInvolved, name='GetInvolved'),
+    path('resources', views.resources, name='resources'),
+    path('programs', views.programs, name='programs'),
 ]
--- app/myapp/views.py
+++ app/myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'Home.html')
+    return render(request, 'Home.html', {'active_page': 'home'})
 
 def meeting(request):
     return render(request, 'meetings.html')
@@ -11,14 +11,26 @@ def meeting_details(request):
     return render(request, 'meeting-details.html')
 
 def aboutus(request):
-    return render(request, 'Aboutus.html')
+    return render(request, 'Aboutus.html', {'active_page': 'about'})
 
 def contactus(request):
-    flag = 0
+    active_page = "contact"
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
         phone = request.POST['phone']
         message = request.POST['message']
         
-    return render(request, 'Contactus.html')
+    return render(request, 'Contactus.html', {'active_page': 'contact'})
+
+def events(request):
+    return render(request, 'Events.html', {'active_page': 'events'})
+
+def GetInvolved(request):
+    return render(request, 'GetInvolved.html', {'active_page': 'GetInvolved'})
+
+def resources(request):
+    return render(request, 'Resources.html', {'active_page': 'resources'})
+
+def programs(request):
+    return render(request,'Programs.html', {'active_page': 'programs'})
